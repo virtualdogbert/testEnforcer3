28a8c033c87c6496d81ced166fd0350e80be0df6
Software Engineering  Geek <virtualdogbert@gmail.com>
2016-08-28 23:07:21 -0400

    Updating for Enforcer 1.3.1

--- grails-app/services/com/security/EnforcerService.groovy
+++ grails-app/services/com/security/EnforcerService.groovy
@@ -23,12 +23,24 @@ import com.virtualdogbert.ast.EnforcerException
 import grails.transaction.Transactional
 import grails.util.Environment
 
+/**
+ * The EnforcerService has one enforce method for enforcing business rules, and is extended by the traits it implements.
+ */
 @Transactional
 class EnforcerService implements RoleTrait,DomainRoleTrait{
 
     def grailsApplication
     def springSecurityService
 
+    /**
+     * The enforce method enforced business rules given 3 closures, a predicate to check, failure and success to rune depending on if the
+     * predicate returns true. The enforce method will run if the environment is not TEST or the grailsApplication.config.enforcer.enabled is
+     * set true
+     *
+     * @param predicate a closure that when run it's result will be checked using groovy's truth model and  if true the success closure is run else the failure closure is run.
+     * @param failure the failure closure to run if the predicate results false, this is defaulted to throw new EnforcerException("Access Denied")
+     * @param success the success closure to run if  the predicate results true, this is defaulted to return true
+     */
     def enforce(Closure predicate, Closure failure = { -> throw new EnforcerException("Access Denied") }, Closure success = { -> true }) {
 
         if (Environment.current != Environment.TEST || grailsApplication.config.enforcer.enabled) {
@@ -43,5 +55,17 @@ class EnforcerService implements RoleTrait,DomainRoleTrait{
             }
         }
 
+    }
+
+    /**
+     * this is used to filter the values of a return statement. This is meant to be used by the ReinforceFilter AST transform
+     *
+     * @param filter a closure that takes and object value, and filters it, and returns the filtered value.
+     * @param value the object to be filtered.
+     *
+     * @return the filtered value.
+     */
+    Object ReinforceFilter(Closure filter, Object value) {
+        filter(value)
     }
 }
